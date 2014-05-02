22684886ef98798ee51ca92796301fa3fceca28e
Sultanxda <sultanxda@gmail.com>
2014-05-01 18:46:25 -0700

    msm_thermal: Make thermal thresholds stricter
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -23,16 +23,19 @@
 #define DEF_TEMP_SENSOR      0
 
 //max thermal limit
-#define DEF_ALLOWED_MAX_HIGH 81
+#define DEF_ALLOWED_MAX_HIGH 80
+#define DEF_RESET_MAX_HIGH   75
 #define DEF_ALLOWED_MAX_FREQ 384000
 
 //mid thermal limit
-#define DEF_ALLOWED_MID_HIGH 78
-#define DEF_ALLOWED_MID_FREQ 648000
+#define DEF_ALLOWED_MID_HIGH 65
+#define DEF_RESET_MID_HIGH   61
+#define DEF_ALLOWED_MID_FREQ 972000
 
 //low thermal limit
-#define DEF_ALLOWED_LOW_HIGH 75
-#define DEF_ALLOWED_LOW_FREQ 972000
+#define DEF_ALLOWED_LOW_HIGH 56
+#define DEF_RESET_LOW_HIGH   53
+#define DEF_ALLOWED_LOW_FREQ 1188000
 
 //Sampling interval
 #define DEF_THERMAL_CHECK_MS 1000
@@ -64,15 +67,15 @@ static struct msm_thermal_tuners {
 	unsigned int check_interval_ms;
 } msm_thermal_tuners_ins = {
 	.allowed_max_high = DEF_ALLOWED_MAX_HIGH,
-	.allowed_max_low = (DEF_ALLOWED_MAX_HIGH - 1),
+	.allowed_max_low = DEF_RESET_MAX_HIGH,
 	.allowed_max_freq = DEF_ALLOWED_MAX_FREQ,
 
 	.allowed_mid_high = DEF_ALLOWED_MID_HIGH,
-	.allowed_mid_low = (DEF_ALLOWED_MID_HIGH - 1),
+	.allowed_mid_low = DEF_RESET_MID_HIGH,
 	.allowed_mid_freq = DEF_ALLOWED_MID_FREQ,
 
 	.allowed_low_high = DEF_ALLOWED_LOW_HIGH,
-	.allowed_low_low = (DEF_ALLOWED_LOW_HIGH - 2),
+	.allowed_low_low = DEF_RESET_LOW_HIGH,
 	.allowed_low_freq = DEF_ALLOWED_LOW_FREQ,
 
 	.check_interval_ms = DEF_THERMAL_CHECK_MS,
