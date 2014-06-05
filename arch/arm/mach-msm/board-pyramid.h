ecce70ea21a19404062f86a1ca55186e41f021a7
Sultanxda <sultanxda@gmail.com>
2014-06-04 21:46:04 -0700

    pyramid: Remove Ion writeback heap
    MSM8X60 doesn't support writeback. Use this space instead to increase SF heap size by 1MB and RAM console size by 2MB.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- arch/arm/mach-msm/board-pyramid.h
+++ arch/arm/mach-msm/board-pyramid.h
@@ -27,7 +27,7 @@
 #define MSM_RAM_CONSOLE_SIZE	MSM_HTC_RAM_CONSOLE_SIZE
 
 /* Memory map */
-#define MSM_ION_HEAP_NUM      7
+#define MSM_ION_HEAP_NUM      6
 
 #define MSM_SMI_BASE          0x38000000
 #define MSM_SMI_SIZE          0x4000000
@@ -35,7 +35,7 @@
 #define MSM_FB_SIZE           roundup((960 * ALIGN(540, 32) * 4 * 3), 4096)
 #define MSM_ION_WB_SIZE       0x300000
 #define MSM_ION_AUDIO_SIZE    0x500000
-#define MSM_ION_SF_SIZE       0x2A00000
+#define MSM_ION_SF_SIZE       0x2B00000
 #define MSM_ION_MM_FW_SIZE    0x200000
 #define MSM_ION_MM_SIZE       0x3D00000
 #define MSM_ION_MFC_SIZE      0x100000
@@ -43,9 +43,8 @@
 #define MSM_ION_MM_FW_BASE    0x38000000
 #define MSM_ION_MM_BASE       0x38200000
 #define MSM_ION_MFC_BASE      0x3BF00000
-#define MSM_ION_SF_BASE       0x40400000
+#define MSM_ION_SF_BASE       0x40300000
 #define MSM_ION_AUDIO_BASE    0x45C00000
-#define MSM_ION_WB_BASE       0x46400000
 
 #define PHY_BASE_ADDR1        0x48000000
 #define SIZE_ADDR1            0x28000000
