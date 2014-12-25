1abfa25107f7f0eb32db29434fb079b2714e6d2e
Boy Petersen <boypetersen@gmail.com>
2014-12-25 22:58:00 +0100

    Revert "cpu_input_boost: Add keypad boosting"
    This reverts commit e1eb9727460bbc7fb24193506f8c4aba6e900e78.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -195,11 +195,6 @@ static const struct input_device_id cpu_boost_ids[] = {
 		.absbit = { [BIT_WORD(ABS_X)] =
 			BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
 	},
-	/* keypad */
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-		.evbit = { BIT_MASK(EV_KEY) },
-	},
 	{ },
 };
 
